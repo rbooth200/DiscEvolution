478c61e37678476e6de1f46e2eeb1f71880b16fa
Richard Booth <richardabooth@gmail.com>
2018-05-20 22:39:26 +0100

    Fixed minor code style errors

--- src/chemistry/krome_chem.py
+++ src/chemistry/krome_chem.py
@@ -15,7 +15,7 @@ from .base_chem import ChemicalAbund
 from pykrome import PyKROME
 
 # Alias for ctypes by reference
-byref = lambda x: ctypes.byref(ctypes.c_double(x))
+def byref(x): return ctypes.byref(ctypes.c_double(x))
 
 _krome = PyKROME(path_to_lib=KROME_PATH)
 _krome.lib.krome_init()
@@ -69,10 +69,10 @@ class KromeMolecularIceAbund(object):
     """Wrapper for holding the fraction of species on/off the grains"""
     def __init__(self, gas=None, ice=None):
         self.gas = gas
-        self.ice = ice    
-        
+        self.ice = ice
 
-        
+
+
 ################################################################################
 # Wrapper for KROME Chemistry solver
 ################################################################################
@@ -88,7 +88,7 @@ class KromeChem(object):
         self._amin = byref(amin)
         self._amax = byref(amax)
         self._phi  = byref(phi)
-        
+
     def update(self, dt, T, rho, dust_frac, chem):
         """Integrate the chemistry for time dt"""
 
@@ -100,20 +100,20 @@ class KromeChem(object):
         m_ice = chem.ice.masses
 
         n = np.empty(len(m_gas) + len(m_ice), dtype='f8')
-        
+
         gas_data = chem.gas.data.T
         ice_data = chem.ice.data.T
         for i in range(len(T)):
             Ti, rho_i, eps_i = T[i], rho[i], dust_frac[i]
 
             nH = rho_i / _krome.krome_p_mass
-            
+
             n_gas = (gas_data[i] / m_gas) * nH
             n_ice = (ice_data[i] / m_ice) * nH
 
             n[_krome_gas] = n_gas
             n[_krome_ice] = n_ice
-            
+
             _krome.lib.krome_init_dust_distribution(n, eps_i,
                                                     self._amin, self._amax,
                                                     self._phi)
@@ -123,7 +123,7 @@ class KromeChem(object):
             gas_data[i] = n[_krome_gas] * m_gas / nH
             ice_data[i] = n[_krome_ice] * m_ice / nH
 
-            
+
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
@@ -143,21 +143,21 @@ if __name__ == "__main__":
 
     Mdot = 1e-8 * (Msun/(2*np.pi)) / AU**2
     Rd = 100.
-    
+
     d2g = 0.01
-    
+
 
     CO_frac = 1e-4
 
     grid = Grid(Rmin, Rmax, Ncell, spacing='log')
     R = grid.Rc
-    
+
     eos = LocallyIsothermalEOS(SimpleStar(), cs0, q, alpha)
     eos.set_grid(grid)
-    
+
     Sigma = (Mdot / (3 * np.pi * eos.nu)) * np.exp(-R/Rd)
     rho = Sigma / (np.sqrt(2 * np.pi) * eos.H * AU)
-    
+
     T = eos.T
     dust_frac = np.ones_like(Sigma) * d2g
 
@@ -165,10 +165,10 @@ if __name__ == "__main__":
     ice = KromeIceAbund(Ncell)
 
     abund = KromeMolecularIceAbund(gas,ice)
-    
+
     abund.gas['CO'] = CO_frac*gas.mass('CO')
     abund.ice['CO'] = 0.
-    
+
     times = np.array([1e0, 1e2, 1e4, 1e6])*2*np.pi
 
     KC = KromeChem()
@@ -190,13 +190,13 @@ if __name__ == "__main__":
     l, = plt.loglog(R, abund.gas.number_abund('CO'), ls='-',
                     label=str(0.) + 'yr')
     plt.loglog(R, abund.ice.number_abund('CO'), ls='--', c=l.get_color())
-    
+
     t = 0.
     for ti in times:
         dt = ti - t
         KC.update(dt, T, rho, dust_frac, abund)
         t = ti
-        
+
         l, = plt.loglog(R, abund.gas.number_abund('CO'), ls='-',
                         label=str(round(t/(2*np.pi),2)) + 'yr')
         plt.loglog(R, abund.ice.number_abund('CO'), ls='--',
