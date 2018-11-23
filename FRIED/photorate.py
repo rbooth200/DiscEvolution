327e850fc8c169030d1df92862c506ef9c95d506
Andrew Sellek <ads79@calx024.ast.cam.ac.uk>
2018-11-23 01:49:07 +0000

    Added interpolation on surface density and radius

--- FRIED/photorate.py
+++ FRIED/photorate.py
@@ -5,30 +5,50 @@ import os
 
 # Import data from FRIED table (Haworth et al 2018)
 # Data listed as M_star, UV, M_disc, Sigma_disc, R_disc, M_dot
-# Take M_star, UV, M_disc, R_disc to build parameter space
-grid_inputs = np.loadtxt(os.environ['DISC_CODE_ROOT']+'/FRIED/friedgrid.dat',skiprows=1,usecols=(0,1,2,4))
+# Take M_star, UV, M_disc, Sigma_disc, R_disc to build parameter space
+grid_parameters = np.loadtxt(os.environ['DISC_CODE_ROOT']+'/FRIED/friedgrid.dat',skiprows=1,usecols=(0,1,2,3,4))
 # Import M_dot
 grid_rate = np.loadtxt(os.environ['DISC_CODE_ROOT']+'/FRIED/friedgrid.dat',skiprows=1,usecols=5)
 
 # Function to return the mass loss rate by interpolating the mass loss rate on the UV flux, disc mass and disc radius for a stellar mass in the grid (FAST, but restrictive)
 # Acceptable masses are 0.05, 0.1, 0.3, 0.5, 0.8, 1.0, 1.3, 1.6, 1.9 times solar
-def PE_rate(M_star = 1.0, UV = 10, M_disc = 1, R_disc = 400):
+def PE_rate_M(M_star = 1.0, UV = 10, M_disc = 1, R_disc = 400):
+	grid_inputs = grid_parameters[:,(1,2,4)]
 	query_inputs = (UV, M_disc, R_disc)
 	grid_rate_exp = np.power(10,grid_rate)
-	select_mass = (np.abs(grid_inputs[:,0] - M_star)<0.001)
+
+	select_mass = (np.abs(grid_parameters[:,0] - M_star)<0.001)
 	if (np.sum(select_mass)==0):
 		print("Invalid Mass")		
 		return -1
 	else:
-		M_dot = interpolate.griddata(grid_inputs[select_mass,1:4],grid_rate_exp[select_mass],query_inputs,method='linear')
+		M_dot = interpolate.griddata(grid_inputs[select_mass,:],grid_rate_exp[select_mass],query_inputs,method='linear')
 		return M_dot
 
 # Function to return the mass loss rate by interpolating the mass loss rate on the UV flux, disc mass and disc radius and stellar mass (SLOW)
-def PE_rate_M(M_star = 1.0, UV = 10, M_disc = 1, R_disc = 400):
+# Currently disagreeing with above for reasons unknown
+def PE_rate_M_S(M_star = 1.0, UV = 10, M_disc = 1, R_disc = 400):
+	grid_inputs = grid_parameters[:,(0,1,2,4)]
 	query_inputs = (M_star, UV, M_disc, R_disc)
 	grid_rate_exp = np.power(10,grid_rate)
-	M_dot = interpolate.griddata(grid_inputs,grid_rate,query_inputs,method='linear')
+
+	M_dot = interpolate.griddata(grid_inputs,grid_rate_exp,query_inputs,method='linear')
 	return M_dot
+
+# Function to return the mass loss rate by interpolating the mass loss rate on the UV flux, gas surface density and disc radius for a stellar mass in the grid (FAST, but restrictive)
+# Acceptable masses are 0.05, 0.1, 0.3, 0.5, 0.8, 1.0, 1.3, 1.6, 1.9 times solar
+def PE_rate_D(M_star = 1.0, UV = 10, Sigma_G = 1, R_disc = 400):
+	grid_inputs = grid_parameters[:,(1,3,4)]
+	query_inputs = (UV, Sigma_G, R_disc)
+	grid_rate_exp = np.power(10,grid_rate)
+
+	select_mass = (np.abs(grid_parameters[:,0] - M_star)<0.001)
+	if (np.sum(select_mass)==0):
+		print("Invalid Mass")		
+		return -1
+	else:
+		M_dot = interpolate.griddata(grid_inputs[select_mass,:],grid_rate_exp[select_mass],query_inputs,method='linear')
+		return M_dot
 
 # Function for comparing the interpolated values calculated here, with the ones from www.friedgrid.com/Tool, which must be manually downloaded and saved into a file. 
 def compareinterp(query_inputs=(1.0,1.2,123),downloadedrates='FRIEDinterp.dat'):
@@ -52,6 +72,12 @@ def compareinterp(query_inputs=(1.0,1.2,123),downloadedrates='FRIEDinterp.dat'):
 if __name__ == "__main__":
 	import sys
 	query_inputs = (float(sys.argv[1]),float(sys.argv[2]),float(sys.argv[3]),float(sys.argv[4]))
-	print(PE_rate(*query_inputs))
+	print(PE_rate_M(*query_inputs))
+	
+	#print(PE_rate_M_S(*query_inputs))
+	
+	#sigma_R = float(sys.argv[3]) * 1.9e30 / (2*np.pi*(float(sys.argv[4])*1.5e13)**2)
+	#query_2 = (float(sys.argv[1]),float(sys.argv[2]),sigma_R,float(sys.argv[4]))
+	#print(PE_rate_D(*query_2))
 
 	#compareinterp()
