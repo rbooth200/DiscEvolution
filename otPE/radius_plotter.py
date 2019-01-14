b04b2d95322c5ae5458a67808cf1d60de778f765
Andrew Sellek <ads79@calx053.ast.cam.ac.uk>
2019-01-14 14:21:06 +0000

    Converting photoevaporation routines to work in presence of dust, but not yet removing entrained material

---
+++ otPE/radius_plotter.py
@@ -0,0 +1,9 @@
+from run_model import timeplot
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--model", "-m", type=str, default=DefaultModel)
+args = parser.parse_args()
+model = json.load(open(args.model, 'r'))
+
+inputdata = np.loadtxt
